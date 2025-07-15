63a09006025b9eef372f3a057a6a38eb28a87eb7
Comdev2k23 <comdev084@gmail.com>
2025-07-15 14:46:06 +0800

    installed nativewind

---
+++ components/SafeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function SafeScreen({children}: {children : React.ReactNode}) {
+  return (
+    <View>
+      
+    </View>
+  )
+}
