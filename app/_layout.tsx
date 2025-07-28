88d72cbfcd082e1dc4c912c5a5f6263cf80c1324
Comdev2k23 <comdev084@gmail.com>
2025-07-28 14:08:09 +0800

    clean update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,18 @@ import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 import '../global.css';
 
 export default function RootLayout() {
   return (
       <ClerkProvider tokenCache={tokenCache}>
-      <SafeScreen>
-        <Slot />
-      </SafeScreen>
-     <StatusBar style='dark'/>
+      <View style={{ flex: 1 }}>
+        <SafeScreen>
+          <Slot />
+        </SafeScreen>
+        <StatusBar style="dark" />
+      </View>
     </ClerkProvider>
   )
 }
